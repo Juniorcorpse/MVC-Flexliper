96930b9db51544ee9a47be45d0efa91e4ce4e3c4
JrSouza <abbathdeath@gmail.com>
2020-01-22 10:55:30 -0300

    autoload
    verifica se da centro essas mudanças antes de aceitar!

--- index.php
+++ index.php
@@ -1,18 +1,19 @@
 <?php
-    require "config.php";
+    require "config.php";//tal vez precise mudar essa linha para require __DIR__.'config.php'
     spl_autoload_register(function ($class){
-        if(strpos($class, "Controller") > -1) {
+        //if(strpos($class, "Controller") > -1) {
             if(file_exists("controllers/".$class.".php")){
-                require "controllers/".$class.".php";
+                require_once 'controllers/'.$class.'.php';
             }
-        }
+       // }
         else if(file_exists("models/".$class.".php")){
-            require "models/".$class.".php";
+            require_once 'models/'.$class.'.php';
         }
         else {
-            require "core/".$class.".php";
+            require_once 'core/'.$class.'.php';
         }
     });
     $core = new Core();
     $core->run();
 ?>  
+//verifica se da centro essas mudanças antes de aceitar!
