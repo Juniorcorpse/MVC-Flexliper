34ae852858d2dbeb7b81c1e0fec67d8e2bbfd5da
JrSouza <abbathdeath@gmail.com>
2020-01-22 10:44:31 -0300

    $db
    aplicar global $db

--- config.php
+++ config.php
@@ -3,15 +3,25 @@
     global $config;
     $config = array();
     if(ENVIRONMENT == "development"){
-        $config["dbname"] = "flexliper";
+        define("BASE_URL", "http://amintoo_completo_do_projeto");//mudar url aqui
+        $config["dbname"] = "flexliper"        
         $config["dbuser"] = "root";
         $config["dbpass"] = "";
         $config["host"] = "127.0.0.1";
     }
     else {
+        define("BASE_URL", "http://amintoo_completo_do_projeto");//mudar url aqui
         $config["dbname"] = "flexliper";
         $config["dbuser"] = "root";
         $config["dbpass"] = "";
         $config["host"] = "127.0.0.1";
     }
+
+global $db;
+try {
+    $db = new PDO("mysql:dbname=".$config['dbname'].";host=".$config['host'],$config['dbuser'],$config['dbpass']);
+} catch(PDOException $e) {
+    echo "ERRO: ".$e->getMessage();
+    exit;
+}
 ?>
